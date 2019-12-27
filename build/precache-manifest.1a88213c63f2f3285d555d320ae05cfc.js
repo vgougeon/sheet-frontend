6f9369de96bd38f2c1d82da53037d83680826f7c
Vincent GOUGEON <gougeonvincent31@outlook.fr>
2019-12-27 15:16:25 +0100

    Build for github

---
+++ build/precache-manifest.1a88213c63f2f3285d555d320ae05cfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35244568bd333e873541fd3a14030dea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e99e2d233cc0b29bc938",
+    "url": "/static/css/main.ef3b396d.chunk.css"
+  },
+  {
+    "revision": "176b1b2f96de8cb7eeed",
+    "url": "/static/js/2.88ec2ddb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.88ec2ddb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e99e2d233cc0b29bc938",
+    "url": "/static/js/main.f98891b7.chunk.js"
+  },
+  {
+    "revision": "d4787e2102fccb57d049",
+    "url": "/static/js/runtime-main.b5d59334.js"
+  }
+]);
